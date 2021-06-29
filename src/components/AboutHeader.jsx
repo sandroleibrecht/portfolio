5a08dcd65343cce931073e5b808ae8270fe110ad
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-06-29 10:47:55 +0200

    some basic header alignments

--- src/components/AboutHeader.jsx
+++ src/components/AboutHeader.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 // Styling & Animations
 import styled from 'styled-components';
+import { Icon } from '@material-ui/core';
+import { GitHub, LinkedIn } from '@material-ui/icons';
 
 function AboutHeader() {
   return (
     <HeaderContainer>
       <InfoContainer>
-        <h1>Hey i'm Sandro</h1>
+        <div>
+          <h3>Hello !</h3>
+          <h2>I'm Sandro</h2>
+          <h4>A passionate <span>Web Developer</span> with a desire to learn</h4>
+          <Icon><GitHub/></Icon>
+          <Icon><LinkedIn/></Icon>
+        </div>
       </InfoContainer>
       <DesignContainer>
-        Img
+        <div></div>
       </DesignContainer>
     </HeaderContainer>
   );
@@ -20,6 +28,7 @@ export default AboutHeader;
 // Styled Components
 const HeaderContainer = styled.header`
   background-color: #f7f7f7c1;
+  padding: calc(10% + 1vh);
   width: 100vw;
   min-height: calc(100vh - 66px);
   display: flex;
@@ -30,13 +39,36 @@ const HeaderContainer = styled.header`
 
 const InfoContainer = styled.div`
   flex: 1;
-  padding: 1rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   
-  h1{
+  h3{
+    font-size: 2.5rem;
+  }
+  h2{
     font-size: 5rem;
+  }
+  h4{
+    font-weight: 400;
+    font-size: 1.1rem;
+    margin-left: .25rem;
+    color: #b3b3b3;
+    span{
+      color: #4D8DF7;
+    }
   }
 `;
 
 const DesignContainer = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div{
+    width: 30rem;
+    height: 120px;
+    border: 1px solid #333;
+  }
 `;

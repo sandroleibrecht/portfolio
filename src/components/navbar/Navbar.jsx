46269b78e3502c24f40302289a96c766b9d90067
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-07-01 23:31:02 +0200

    made fixed to top navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -27,24 +27,37 @@ function Navbar() {
   };
 
   return (
-    <StyledNavbar>
-      <Link to='/'>
-        <Logo src={logoImage} menuOpen={menuOpen} alt="Page Logo" />
-      </Link>
-      <MenuIcon icon={faBars} onClick={ handleMenuOpen }/>
-      <NavInline/>
-      <NavSide/>
-    </StyledNavbar>
+    <>
+      <TopSpacing/>
+      <StyledNavbar>
+        <Link to='/'>
+          <Logo src={logoImage} menuOpen={menuOpen} alt="Page Logo" />
+        </Link>
+        <MenuIcon icon={faBars} onClick={ handleMenuOpen }/>
+        <NavInline/>
+        <NavSide/>
+      </StyledNavbar>
+    </>
   );
 };
 
 // Styled Components
+const TopSpacing = styled.div`
+  width: 100vw;
+  height: 66px;
+`;
+
 const StyledNavbar = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1rem 9.5vw;
   width: 100vw;
+  background-color: #fff;
+  box-shadow: 0px 12px 16px -10px #e3e3e3a9;
+  position: fixed;
+  top: 0;
+  left: 0;
 
   @media (max-width: 630px){
     padding-left: 2.3rem;
@@ -53,6 +66,8 @@ const StyledNavbar = styled.nav`
 `;
 
 const Logo = styled.img`
+  position: relative;
+  top: 2px;
   width: 30px;
   height: 30px;
   margin-right: 1.5rem;

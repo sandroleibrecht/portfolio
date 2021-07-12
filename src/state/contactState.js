96313a68a49b2cc04ae89638b767f41029b77628
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-07-12 13:51:50 +0200

    contact state updated

--- src/state/contactState.js
+++ src/state/contactState.js
@@ -5,13 +5,30 @@ export const setFocus = ( status ) => {
     payload: { status }
   }
 };
+export const setValue = ( prop, value ) => {
+  prop = prop.toLowerCase();
+  return{
+    type: 'SET_VALUE',
+    payload: { prop, value }
+  }
+};
 
 // Reducer
-const initialState = { formFocusing: false };
+const initialState = {
+  formFocusing: false,
+  values: {
+    name: '',
+    mail: '',
+    message: ''
+  }
+};
 const ContactReducer = ( state = initialState, action ) => {
+  const data = action.payload;
   switch( action.type ){
     case 'SET_FOCUS':
-      return { ...state, formFocusing: action.payload.status };
+      return { ...state, formFocusing: data.status };
+    case 'SET_VALUE':
+      return { ...state , values: { ...state.values, [data.prop]: data.value }};
     default:
       return { ...state };
   }

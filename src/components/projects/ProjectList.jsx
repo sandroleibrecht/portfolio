bd192930e54a851b587d61de536f7c32b6d40ba9
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-07-13 20:50:20 +0200

    hardcode display only first project

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import styled from 'styled-components';
 // Project Component
 import Project from './Project';
-// Projects Data
-import projects from '../../assets/data/ProjectList';
+// Redux
+import { useSelector } from 'react-redux';
 
 function ProjectList() {
+  const {projectList} = useSelector( state => state.projects );
   return (
     <List>
-      {projects.map( project => <Project {...project} key={project.id} /> )}
+      <Project {...projectList[0]} key={projectList[0].id} />
+      {/* {projects.map( project => <Project {...project} key={project.id} /> )} */}
     </List>
   )
 };

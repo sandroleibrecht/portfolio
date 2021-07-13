1e7b7226f47a8757c56af35bd53704fab74254e2
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-07-13 14:06:32 +0200

    projects page translation file setup

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// Text Content
+import pageText from '../assets/translations/projects.json';
 // Framer Motion
 import { motion } from 'framer-motion';
 import { pageAnimation } from '../assets/styling/GlobalStyles';
@@ -6,8 +8,14 @@ import { pageAnimation } from '../assets/styling/GlobalStyles';
 import Footer from '../components/footer/Footer';
 // Scroll Top
 import ScrollTop from '../assets/util/ScrollTop';
+// Redux
+import { useSelector } from 'react-redux';
 
 function ProjectsPage() {
+
+  const { selectedLanguage } = useSelector(state => state.language);
+  const text = pageText[selectedLanguage];
+
   return (
     <motion.div variants={pageAnimation} initial="hidden" animate="show" exit="exit">
       Projects Page

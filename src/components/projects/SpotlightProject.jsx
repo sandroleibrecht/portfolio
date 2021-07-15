b4c1f63d1cb82fcde39c1d4a7b2580645da1e857
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-07-16 01:28:15 +0200

    margin between projects reduced

--- src/components/projects/SpotlightProject.jsx
+++ src/components/projects/SpotlightProject.jsx
@@ -39,7 +39,7 @@ function SpotlightProject( project ) {
 
 // Styled Components
 const ProjectContainer = styled.div`
-  margin-bottom: 8rem;
+  margin-bottom: 5rem;
   box-shadow: 0 0 15px 1px #e7e7e7;
   border-radius: 12px;
   padding: 1rem;

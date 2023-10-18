5eb9c8a8a132ce02669aea1af29f5f52f8306479
sandropernerstorfer <sandro.pernerstorfer@gmail.com>
2023-10-18 11:15:49 +0200

    about section header UI updates

--- src/components/about/AboutHeader.jsx
+++ src/components/about/AboutHeader.jsx
@@ -9,23 +9,28 @@ import Grid from '@material-ui/core/Grid';
 
 function AboutHeader({ textContent }) {
 
-  function showImage(e){
-    e.target.classList.add('image-visible');
-  };
-
   return (
     <HeaderContainer container spacing={0} component="header">
-      <InfoContainer item xs={12} sm={12} md={6} lg={6}>
+      <InfoContainer item xs={12} sm={12} md={12} lg={12}>
         <div>
-          <h3>{textContent.greeting}</h3>
-          <h2>{textContent.name}&nbsp;!</h2>
-          <h4>{textContent.quote1}<span>{textContent.title}</span>{textContent.quote2}</h4>
+          <h2>{textContent.greeting}</h2>
+          <h4>
+            {textContent.quote1}
+            <span className="colorBrace1">{textContent.braceOpen}</span>
+            <span>{textContent.title1}</span>
+            <span className="colorBrace1">{textContent.braceClose}</span>
+            <span>{textContent.connector}</span>
+            <span className="colorBrace2">{textContent.braceOpen}</span>
+            <span>{textContent.title2}</span>
+            <span className="colorBrace2">{textContent.braceClose}</span>
+            {textContent.quote2}
+          </h4>
+          <hr/>
+          <h4>{textContent.text1}</h4>
+          <h4>{textContent.text2}</h4>
           <GitHub onClick={ () => openNewTab('https://github.com/sandropernerstorfer') }/>
         </div>
       </InfoContainer>
-      <DesignContainer item xs={12} sm={12} md={6} lg={6}>
-          <img onLoad={showImage} src={process.env.PUBLIC_URL+'/img/app/headerImage.svg'} alt="code window"/>
-      </DesignContainer>
     </HeaderContainer>
   );
 };
@@ -36,7 +41,7 @@ export default AboutHeader;
 const HeaderContainer = styled(Grid)`
   padding: 5vw calc(9% + 1vh);
   width: 100vw;
-  box-shadow: inset 0px -11px 30px -12px #E3E3E3;
+  background-color: #5f5fcd;
 
   @media (max-width: 959px){
     padding-top: 4rem;
@@ -51,32 +56,60 @@ const InfoContainer = styled(Grid)`
   justify-content: center;
   align-items: center;
 
+  hr{
+    height: 1px;
+    background-color: #dadada;
+    border: none;
+    margin: 20px 0;
+  }
+
+  div{
+    max-width: 950px;
+    padding: 1rem;
+    border-radius: 20px;
+  }
+
+  .sideBar{
+    background-color: #fff;
+    width: 10px;
+    height: 100%;
+  }
+
   h2,h3,h4{
-    color: #464444;
+    color: white;
   }
 
   h3{
     font-size: 2.5rem;
   }
   h2{
-    font-size: 5rem;
+    font-size: 3.5rem;
   }
   h4{
     font-weight: 400;
     font-size: 1.1rem;
     margin: .5rem 0 1rem .25rem;
-    color: #999898;
+    color: white;
     span{
-      color: var(--blue);
+      color: white;
+      font-weight: 500;
+      font-size: 1rem;
+      font-family: monospace;
+    }
+    .colorBrace1{
+      color: #ff00ff;
+    }
+    .colorBrace2{
+      color: #ffe600;
     }
   }
   svg{
-    color: #fff;
+    color: #454545;
     font-size: 2rem;
     border-radius: 15px;
     margin: 1rem 1.5rem 1rem .2rem;
-    padding: 4px;
-    background-color: var(--blueLight);
+    padding: 3px;
+    background-color: white;
     cursor: pointer;
     transition: transform .2s;
 
@@ -87,46 +120,10 @@ const InfoContainer = styled(Grid)`
     }
   }
 
-  @media (max-width: 1370px){
-    h3  { font-size: 2.5vw;  }
-    h2  { font-size: 5vw;    }
-    h4  { font-size: 1.4vw;  }
-    svg { font-size: 2rem;    }
-  }
-
   @media (max-width: 959px){
-    h3  { font-size: 5vw;   }
-    h2  { font-size: 8vw;  }
-    h4  { font-size: 2.5vw; }
-    svg { font-size: 2rem; margin-left: 1.5rem; }
-    text-align: center;
-  }
-
-  @media (max-width: 530px){
     h3  { font-size: 1.5rem;  }
     h2  { font-size: 2.5rem;  }
     h4  { font-size: 1rem; }
     svg { font-size: 2rem;  }
   }
-`;
-
-const DesignContainer = styled(Grid)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-    img{
-      opacity: 0;
-      width: 100%;
-      height: auto;
-      transition: all 1.5s;
-    }
-
-    .image-visible{
-      opacity: 1;
-    }
-
-    @media (max-width: 959px){
-      padding: 3rem 2rem;
-    }
 `;

235626475e33e25bc8689227259715c30a6a6b0f
sandroleibrecht <sandro.leibrecht@gmail.com>
2025-11-07 11:28:09 +0100

    add image preload for project details

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -3,34 +3,49 @@ import SpotlightProject from './SpotlightProject';
 import OtherProject from './OtherProject';
 import Line from '../Line';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
-function ProjectList({text}) {
-  const {projectList} = useSelector( state => state.projects );
-  
+function ProjectList({ text }) {
+  const { projectList } = useSelector(state => state.projects);
+
   const spotlightProjects = [];
   const otherProjects = [];
 
-  projectList.forEach( project => {
-    project.spotlight ? spotlightProjects.push(project) : otherProjects.push(project);
-  });
+  projectList.forEach(project => project.spotlight ? spotlightProjects.push(project) : otherProjects.push(project));
+
+  // preload project images for detail view 
+  useEffect(() => {
+    projectList.forEach(project => {
+      if (project.screenshots?.length) {
+        project.screenshots.forEach(src => {
+          const img = new Image();
+          img.src = src;
+        });
+      }
+    });
+  }, [projectList]);
 
   return (
     <>
       <List>
-        { spotlightProjects.map(project => <SpotlightProject {...project} key={project.id} />) }
+        {spotlightProjects.map(project => (
+          <SpotlightProject {...project} key={project.id} />
+        ))}
       </List>
-      <Line/>
+      <Line />
       <OtherProjects>
         <div>
           <h3>{text.heading}</h3>
         </div>
         <div className="projectContainer">
-          { otherProjects.map(project => <OtherProject {...project} key={project.id} />) }
+          {otherProjects.map(project => (
+            <OtherProject {...project} key={project.id} />
+          ))}
         </div>
       </OtherProjects>
     </>
-  )
-};
+  );
+}
 
 const List = styled.section`
   display: flex;
@@ -38,27 +53,27 @@ const List = styled.section`
   justify-content: space-evenly;
   align-items: center;
   gap: 1rem;
-  
-  @media (max-width: 1350px){
+
+  @media (max-width: 1350px) {
     flex-direction: column;
   }
 `;
 
 const OtherProjects = styled.section`
   width: 100%;
-  h3{
+
+  h3 {
     text-align: center;
     margin: 4rem 0;
   }
 
-  .projectContainer{
+  .projectContainer {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
     gap: 2rem;
   }
-
 `;
 
 export default ProjectList;
